fe189c400dc5eb6db82d0a2e4d9b0dac384885c2
GiorgiShushanashvili <shushanashvili20@gmail.com>
2023-03-03 22:01:11 +0400

    add new app

---
+++ CommschoolProject/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("enter a word");
+var word = Console.ReadLine();
+Console.ForegroundColor = ConsoleColor.Yellow;
+Console.BackgroundColor = ConsoleColor.Red;
+Console.ResetColor();
+Console.WriteLine(word + " is my name");
+Console.WriteLine(word is "23 years old");
+Console.WriteLine("i live in tbilisi");
+Console.WriteLine("i play footbal as well");
+Console.WriteLine("hello world");
+Console.WriteLine(123456);
+Console.WriteLine(789);
+var sacxovrebeli = "axmeta";
+Console.WriteLine($" {word} is from {sacxovrebeli}");
+
+
+Console.ReadLine();
+
